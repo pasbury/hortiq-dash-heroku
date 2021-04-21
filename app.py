9b1888d110b3c313f42745aff164fbaf99f7b290
Paul Asbury <paul_asbury@hotmail.co.uk>
2021-04-21 21:25:50 +0100

    Test adding scatter plot

--- app.py
+++ app.py
@@ -18,6 +18,13 @@ try:
     interest = json.loads(indata)
 except FileNotFoundError as fnf_error:
     print(fnf_error)
+# prepare dataframe to plot
+d = {k: {'relative_interest': v['relative_interest'], 'yoy_1Y_pct': v['yoy_1Y_pct']} for k, v in interest.items()}
+df = pd.DataFrame(d).transpose()
+df['label'] = df.index
+df = df.query('-0.2 <= yoy_1Y_pct <= 1')
+
+fig = px.scatter(df,x='relative_interest',y='yoy_1Y_pct', text='label', log_x=True, height=800, width = 800)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -25,13 +32,14 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2('Hortiq'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+    dcc.Graph(id='scatter-plot', figure=fig)
 ])
 
 @app.callback(dash.dependencies.Output('display-value', 'children'),
