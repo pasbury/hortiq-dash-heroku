f07ecca18ff66878f029b19b1e868b459ef17505
Paul Asbury <paul_asbury@hotmail.co.uk>
2021-04-21 23:08:17 +0100

    Test filtering plot based on dropdown

--- app.py
+++ app.py
@@ -31,21 +31,24 @@ plant_types = set()
 [plant_types.update(v['plant_types']) for k,v in genera_dict.items()]
 plant_type_options = [ {'label':e, 'value':e} for e in plant_types ]
 
-# get lookup of genera for each plant type
-type_to_genera = {}
-for g,v in genera_dict.items():
-    for t in v['plant_types']:
-        if t not in type_to_genera:
-            type_to_genera[t] = []
-        type_to_genera[t].append(g)
+# get lookup of genera to display, given selected plant types
+def genera_to_show(genera_dict, types):
+    type_to_genera = {}
+    for g, v in genera_dict.items():
+        for t in v['plant_types']:
+            if t not in type_to_genera:
+                type_to_genera[t] = []
+            type_to_genera[t].append(g)
+
+    distinct_genera = set()
+    [distinct_genera.update(type_to_genera[t]) for t in types]
+    return sorted(distinct_genera)
 
 # prepare dataframe to plot
 d = {k: {'relative_interest': v['relative_interest'], 'yoy_1Y_pct': v['yoy_1Y_pct']} for k, v in interest.items()}
 df = pd.DataFrame(d).transpose()
 df['label'] = df.index
 df = df.query('-0.2 <= yoy_1Y_pct <= 1')
-# create the plot
-fig = px.scatter(df,x='relative_interest',y='yoy_1Y_pct', text='label', log_x=True, height=800, width = 800)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -60,14 +63,19 @@ app.layout = html.Div([
         value=['All Types'],
         multi=True
     ),
-    html.Div(id='display-value'),
     dcc.Graph(id='scatter-plot', figure=fig)
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(dash.dependencies.Output('scatter-plot', 'figure'),
+              dash.dependencies.Input('dropdown', 'value'))
+def update_figure(value):
+    # filter dataframe based on selection
+    filter_list = genera_to_show(genera_dict, value)
+    fdf = df[df.index.isin(filter_list)]
+    # create the plot
+    fig = px.scatter(fdf, x='relative_interest', y='yoy_1Y_pct', text='label', log_x=True, height=800, width=800)
+
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
