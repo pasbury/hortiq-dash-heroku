494c5a9c0de10ffb934d698c5ee6cafe933860b6
pasbury <paul_asbury@hotmail.co.uk>
2021-05-12 16:43:49 +0100

    Use property, not name for meta tags

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import dash_bootstrap_components as dbc
 
 external_stylesheets = [dbc.themes.FLATLY] #['https://codepen.io/chriddyp/pen/bWLwgP.css']
 meta_tags_list = [
-    {'name': 'og:title', 'content': 'Hortiq: Horticultural intelligence.'},
-    {'name': 'og:image', 'content': 'assets/hortiq-logo-black.png'},
-    {'name': 'og:url', 'content': 'http://www.hortiq.co.uk/'},
-    {'name': 'og:description', 'content': 'Market intelligence for growers and online retailers in the horticulture industry.'}
+    {'property': 'og:title', 'content': 'Hortiq: Horticultural intelligence.'},
+    {'property': 'og:image', 'content': 'assets/hortiq-logo-black.png'},
+    {'property': 'og:url', 'content': 'http://www.hortiq.co.uk/'},
+    {'property': 'og:description', 'content': 'Market intelligence for growers and online retailers in the horticulture industry.'}
 ]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, title='Hortiq - Horticultural intelligence',
