c9d04cdc134073deaec610f80451ad90e9e698f1
pasbury <paul_asbury@hotmail.co.uk>
2021-05-31 11:09:14 +0100

    Update image t0 1.9:1 aspect ratio

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash_bootstrap_components as dbc
 external_stylesheets = [dbc.themes.FLATLY] #['https://codepen.io/chriddyp/pen/bWLwgP.css']
 meta_tags_list = [
     {'property': 'og:title', 'content': 'Hortiq: Horticultural intelligence.'},
-    {'property': 'og:image', 'content': 'http://www.hortiq.co.uk/assets/hortiq-screenshot.png'},
+    {'property': 'og:image', 'content': 'http://www.hortiq.co.uk/assets/hortiq-screenshot_1.9_1.png'},
     {'property': 'og:url', 'content': 'http://www.hortiq.co.uk/'},
     {'property': 'og:description', 'content': 'Market intelligence for growers and online retailers in the horticulture industry.'}
 ]
